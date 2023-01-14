7dc9e47b12e61a8a9f8ce9532c59858b1083560b
rubyazz <ersultan.abduvalov@gmail.com>
2023-01-14 10:25:48 +0600

    search filter ecode

--- Ecode/views.py
+++ Ecode/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 
 from .models import Code
 from .serializer import EcodeSerializer
@@ -9,3 +9,6 @@ from .serializer import EcodeSerializer
 class EcodeAPIList(generics.ListCreateAPIView):
     queryset = Code.objects.all()
     serializer_class = EcodeSerializer
+
+    search_fields = ['name', 'description', ]
+    filter_backends = (filters.SearchFilter,)

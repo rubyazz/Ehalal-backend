fc0bc1b5bb3b56af3b38f960c471939d69ec61fa
rubyazz <ersultan.abduvalov@gmail.com>
2023-01-14 10:50:19 +0600

    fixed media

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 from .views import ProductAPIList
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('', views.home_view),
     path('article/list/', ProductAPIList.as_view())
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

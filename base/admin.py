f3f50d1baafb2e8af06aa4797a0c9537448575d6
rubyazz <ersultan.abduvalov@gmail.com>
2023-01-14 10:14:05 +0600

    add Ecode app

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import ProductList, ProductGen, ProductItem
+from .models import ProductList, ProductGen, ProductItem, Category, Code
 
 admin.site.register(ProductGen)
 admin.site.register(ProductList)
 admin.site.register(ProductItem)
+admin.site.register(Code)
+admin.site.register(Category)
 
